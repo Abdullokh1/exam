af3fa71f9adda33315d464e3318d450d18b2233f
Abdullokh1 <giyasovabdullo0@gmail.com>
2022-01-19 00:58:10 +0500

    second change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 document.getElementById('darkBtn').addEventListener('click', function(){
   document.body.classList.toggle('dark')
 })
+let hideVersion = document.getElementById('hideContent');
+let openBtn = document.getElementById('searchBtn');
+openBtn.addEventListener('click', function(){
+  hideVersion.classList.add('show')
+})
+let close = document.getElementById('closeBtn');
+
+close.addEventListener('click', function(){
+  hideVersion.classList.remove('show')
+})
